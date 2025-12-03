2c5e7d4e415e4878e5c83b863949726b9359bd78
Arun Prasaad <arunbear@cpan.org>
2025-12-03 12:43:00 +0000

    Remove direct import logic
    Better to have one well tested code path.

--- devel/import_xml.pl
+++ devel/import_xml.pl
@@ -4,7 +4,6 @@ use autodie;
 use Data::Dump 'dump';
 use Getopt::Long;
 use HTTP::Status qw(HTTP_CONFLICT);
-use Mojolicious;
 use Mojo::DOM;
 use Mojo::Util qw(trim);
 use XXX -with => 'Data::Dump';
@@ -48,11 +47,11 @@ sub parse_xml ($xml_file) {
 
     # Extract node data
     my $node = $dom->at('node')
-        or die "No node found in XML file\n";
+        or die "No node found in XML file $xml_file";
 
     my %field;
     $field{node_id} = $node->attr('id')
-        or die "Node ID is required\n";
+        or die "Node ID is required: $xml_file";
 
     for my $attr (qw(title created updated)) {
         $field{$attr} = $node->attr($attr);
@@ -82,107 +81,7 @@ sub parse_xml ($xml_file) {
 }
 
 sub import_node_data ($node_data) {
-    if ($OPT{'use-api'}) {
-        return import_node_data_via_api($node_data);
-    }
-
-    my $pg = get_db_connection();
-    my $db = $pg->db;
-    my $tx = $db->begin;
-    my $rows = 0;
-
-    try {
-        ensure_author_exists($db, $node_data);
-        ensure_node_type_exists($db, $node_data);
-        $rows = insert_node($db, $node_data);
-        $tx->commit;
-    } catch ($error) {
-        my $message = "Failed to import node $node_data->{node_id}: $error\n" . dump($node_data);
-        if ($error =~ /is not present in table \"node\"/) {
-            warn "[Skipping] $message";
-        }
-        else {
-            die "[Stopped] $message";
-        }
-    }
-    return $rows;
-}
-
-sub ensure_node_type_exists ($db, $node_data) {
-    my $results = $db->insert('node_type', {
-        id          => $node_data->{type_id},
-        name        => $node_data->{type_name},
-    }, { on_conflict => undef });
-
-    printf("Rows inserted into node_type: %d\n", $results->rows);
-}
-
-sub ensure_author_exists ($db, $author_data) {
-    my $results = $db->insert('monk', {
-        id           => $author_data->{author_id},
-        username     => $author_data->{author_username},
-        is_anonymous => ($author_data->{author_id} == 961 ? 1 : 0),
-        created_at   => $author_data->{created},
-        updated_at   => $author_data->{created},
-    }, { on_conflict => undef });
-
-    printf("Rows inserted into monk: %d\n", $results->rows);
-    return $results;
-}
-
-sub insert_node ($db, $node_data) {
-    if (!$node_data->{doctext}) {
-        return 0;
-    }
-    if ($node_data->{updated} eq '0000-00-00 00:00:00') {
-        $node_data->{updated} = $node_data->{created};
-    }
-    my $node_results = $db->insert('node', {
-        id           => $node_data->{node_id},
-        node_type_id => $node_data->{type_id},
-        author_id    => $node_data->{author_id},
-        title        => $node_data->{title},
-        doctext      => $node_data->{doctext},
-        reputation   => $node_data->{reputation} // 0,
-        created_at   => $node_data->{created},
-        updated_at   => $node_data->{updated}
-    }, { on_conflict => undef });
-
-    printf("Rows inserted into node: %d\n", $node_results->rows);
-
-    if ($node_data->{type_id} == 11) {
-        insert_note($db, $node_data);
-    }
-    return $node_results->rows;
-}
-
-sub insert_note ($db, $node_data) {
-    my $root_node   = $node_data->{root_node};
-    my $parent_node = $node_data->{parent_node};
-
-    my @path_info = ($node_data->{node_id});
-    if ($parent_node eq $root_node) {
-        unshift @path_info, $parent_node;
-    }
-    else  {
-        # Get parent's path and append current node ID
-        my $parent = $db->select('note', ['path'], { node_id => $parent_node })->hash;
-        if (!defined $parent) {
-            warn "Parent node $parent_node not found for node $node_data->{node_id}";
-            return;
-        }
-        unshift @path_info, $parent->{path};
-    }
-    my $path = join('.', @path_info);
-
-    my $note_results = $db->insert('note', {
-        node_id     => $node_data->{node_id},
-        root_node   => $root_node,
-        parent_node => $parent_node,
-        path        => $path
-    }, { on_conflict => undef });
-
-    printf("Rows inserted into note: %d\n", $note_results->rows);
+    return import_node_data_via_api($node_data);
 }
 
 # ====== Utility Functions ======
